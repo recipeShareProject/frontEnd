837cab750e7f1421ab41bf766ef5ecb75c673b42
leehyeonj <billaa@naver.com>
2022-05-21 13:06:26 +0900

    feat : 나의 글 > 레시피 디자인

--- src/pages/myPage/RecipeTab.js
+++ src/pages/myPage/RecipeTab.js
@@ -1,48 +1,13 @@
+import {Box} from '@mui/material';
+import SearchResultContent from 'components/search/SearchResultContent';
 import React from 'react';
-import styled from 'styled-components';
 
 function RecipeTab() {
   return (
-    <>
-      <StyleGrid>
-        <StyledCard>
-          <StyledImg></StyledImg>
-          <p>레시피명</p>
-          <p>n분</p>
-        </StyledCard>
-        <StyledCard>
-          <StyledImg></StyledImg>
-          <p>레시피명</p>
-          <p>n분</p>
-        </StyledCard>
-        <StyledCard>
-          <StyledImg></StyledImg>
-          <p>레시피명</p>
-          <p>n분</p>
-        </StyledCard>
-        <StyledCard>
-          <StyledImg></StyledImg>
-          <p>레시피명</p>
-          <p>n분</p>
-        </StyledCard>
-      </StyleGrid>
-    </>
+    <Box mt={4}>
+      <SearchResultContent />
+    </Box>
   );
 }
 
-const StyleGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-
-  & > div:nth-child(odd) {
-    padding-right: 0.5rem;
-  }
-`;
-const StyledCard = styled.div``;
-
-const StyledImg = styled.div`
-  background-color: lightgray;
-  width: 159px;
-  height: 121px;
-`;
 export default RecipeTab;
