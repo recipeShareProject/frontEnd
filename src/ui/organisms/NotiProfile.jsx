ee30ff4b4d4f0237c10fca0b1832d75ff4b34add
gustmd213 <51289147+gustmd213@users.noreply.github.com>
2022-05-24 22:50:18 +0900

    [refactor] 알림 atomic적용

--- src/ui/organisms/NotiProfile.jsx
+++ src/ui/organisms/NotiProfile.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-const NotiProfile = () => {
-  return (
-    <Flex>
-      <Profill>
-        <StyledImg />
-      </Profill>
-      <div>
-        <div>OO님이 나의 글에 댓글을 남겼어요</div>
-        <div>아직도 나눔 하시나요? 저도 받고 싶어요 답 주세요!</div>
 
-        <div>n분 전 </div>
-      </div>
-    </Flex>
+import Wrapper from 'ui/atoms/Wrapper';
+import Typography from 'ui/atoms/Typography';
+import AvataImg from 'ui/atoms/AvataImg';
+import {Black40} from 'assets/colorSet';
+const NotiProfile = ({opacity}) => {
+  return (
+    <Wrapper opacity={opacity} display="flex" margin="1.5rem 0">
+      <AvataImg />
+      <Wrapper>
+        <Typography margin="0 0 4px 0">
+          OO님이 나의 글에 댓글을 남겼어요
+        </Typography>
+        <Typography fontSize="12px" margin="0 0 4px 0">
+          아직도 나눔 하시나요? 저도 받고 싶어요 답 주세요!
+        </Typography>
+
+        <Typography fontSize="12px" color={Black40}>
+          n분 전
+        </Typography>
+      </Wrapper>
+    </Wrapper>
   );
 };
-const StyledImg = styled.div`
-  background-color: lightgray;
-  border-radius: 50%;
-  width: 36px;
-  height: 36px;
-`;
-const Profill = styled.div`
-  margin-right: 1rem;
-`;
-const Flex = styled.div`
-  display: flex;
-  padding: 0.75rem 0;
-`;
 
 export default NotiProfile;
--- src/ui/templates/noti/NotiTemplate.jsx
+++ src/ui/templates/noti/NotiTemplate.jsx
@@ -5,28 +5,31 @@ import HeaderBar from 'ui/templates/header/HeaderBar';
 import Wrapper from 'ui/atoms/Wrapper';
 import Divider from 'ui/atoms/Divider';
 import NotiProfile from 'ui/organisms/NotiProfile';
+import Typography from 'ui/atoms/Typography';
+import NotFound from 'ui/templates/NotFound';
 const NotiTemplate = () => {
   return (
     <React.Fragment>
       <HeaderBar />
       <Wrapper padding="72px 0 0 0">
         <Wrapper padding="0 1rem">
-          <h3>알림</h3>
-          <StyleFlex>
-            <div>비우기</div>
-          </StyleFlex>
+          <Typography margin="1rem 0 0 0" fontSize="20px" fontWeight="700">
+            알림
+          </Typography>
+          <Wrapper display="flex" justify="flex-end">
+            <Typography margin=" 0 0 1rem 0" fontSize="14px" fontWeight="500">
+              비우기
+            </Typography>
+          </Wrapper>
         </Wrapper>
         <Divider />
         <Wrapper padding="0 1rem">
           <NotiProfile />
+          {/* <NotFound desc="새로운 알림이 없어요" /> */}
         </Wrapper>
       </Wrapper>
     </React.Fragment>
   );
 };
-const StyleFlex = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
 
 export default NotiTemplate;
