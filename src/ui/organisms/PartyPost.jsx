1c9b6ecb0719e4b5b82848efe335de70f93aaf8a
gustmd213 <51289147+gustmd213@users.noreply.github.com>
2022-06-01 17:59:35 +0900

    [feat] 나눔완료시 disable처리

--- src/ui/organisms/PartyPost.jsx
+++ src/ui/organisms/PartyPost.jsx
@@ -19,6 +19,34 @@ const PartyPost = ({
     dispatch(getPost(id));
     navigate(`detailParty/${id}`);
   };
+  if (category === '나눔완료') {
+    return (
+      <Wrapper display="flex" margin="1rem 0" disabled="true">
+        <Image
+          width="6.5rem"
+          height="6.5rem"
+          src={thumnail}
+          radius="4px"
+          margin="0 8px 0 0 "></Image>
+        <Wrapper>
+          <Typography color={Colar100} fontSize="12px">
+            {category}
+          </Typography>
+          <Typography fontWeight="bold" fontSize="1rem">
+            {title}
+          </Typography>
+          <Wrapper display="flex">
+            <Typography fontSize="12px" margin="0 8px 0 0">
+              {address}
+            </Typography>
+            <Typography color={Black40} fontSize="12px">
+              {time}
+            </Typography>
+          </Wrapper>
+        </Wrapper>
+      </Wrapper>
+    );
+  }
   return (
     <Wrapper display="flex" margin="1rem 0" _onClick={moveDetailPage}>
       <Image
